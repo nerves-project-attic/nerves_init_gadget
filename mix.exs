58699d162406143fab129abf46cc363c405b2a9f
Frank Hunleth <fhunleth@troodon-software.com>
2017-07-09 15:45:42 -0400

    Update nerves_firmware_ssh ref to hex.pm

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Nerves.InitGadget.Mixfile do
   defp deps do
     [{:nerves_runtime, "~> 0.3"},
      {:nerves_network, github: "nerves-project/nerves_network"},
-     {:nerves_firmware_ssh, github: "fhunleth/nerves_firmware_ssh"},
+     {:nerves_firmware_ssh, "~> 0.1"},
      {:exjsx, "~> 4.0"},
      {:mdns, github: "mobileoverlord/mdns", branch: "linklocal"}]
   end
