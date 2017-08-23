3a1a2ecd5dd6d1ea2e37200eb576b07207174d27
Frank Hunleth <fhunleth@troodon-software.com>
2017-08-23 16:23:16 -0400

    Add a simple module doc

--- lib/nerves_init_gadget.ex
+++ lib/nerves_init_gadget.ex
@@ -1,5 +1,22 @@
 defmodule Nerves.InitGadget do
   @moduledoc """
-  Documentation for Nerves.InitGadget.
+  `nerves_init_gadget` adds a basic level of setup for Nerves devices with USB gadget mode
+  interfaces like the Raspberry Pi Zero. Here are some features:
+
+  * Automatically sets up link-local networking on the USB interface. No DHCP or
+    static IP setup is needed on the host laptop
+  * Sets up mDNS to respond to lookups for `nerves.local`
+  * Pulls in the `nerves_runtime` initialization for things like mounting and
+    fixing the application filesystem
+  * Starts `nerves_firmware_ssh` so that firmware push updates work
+  * If used with [bootloader](https://github.com/nerves-project/bootloader),
+    crashes in your application's initialization won't break firmware updates
+
+  While you'll probably want to create your own device initialization project at
+  some point, this project serves as a great starting point, especially if you're
+  new to Nerves.
+
+  All configuration is handled at compile-time, so there's not an API. See the
+  `README.md` for installation and use instructions.
   """
 end
