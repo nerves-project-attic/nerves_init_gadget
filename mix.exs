edf57a8e14e6f2c3033c96ce7372df6bcc36d5e0
Frank Hunleth <fhunleth@troodon-software.com>
2017-08-25 12:48:33 -0400

    Minor cleanup

--- mix.exs
+++ mix.exs
@@ -1,10 +1,16 @@
 defmodule Nerves.InitGadget.Mixfile do
   use Mix.Project
 
+  @version "0.2.0"
+
+  @description """
+  Easy Nerves initialization for devices with USB gadget interfaces
+  """
+
   def project do
     [app: :nerves_init_gadget,
-     version: "0.1.0",
-     description: "Easy Nerves initialization for devices with USB gadget interfaces",
+     version: @version,
+     description: @description,
      package: package(),
      elixir: "~> 1.4",
      docs: docs(),
@@ -13,23 +19,23 @@ defmodule Nerves.InitGadget.Mixfile do
      deps: deps()]
   end
 
-  def application do
+  def application() do
     [extra_applications: [:logger],
      mod: {Nerves.InitGadget.Application, []}]
   end
 
-  defp package do
+  defp package() do
     %{maintainers: ["Frank Hunleth"],
       licenses: ["Apache-2.0"],
       links: %{"GitHub" => "https://github.com/fhunleth/nerves_init_gadget"}}
   end
 
-  defp docs do
+  defp docs() do
     [main: "readme",
      extras: ["README.md"]]
   end
 
-  defp deps do
+  defp deps() do
     [{:nerves_runtime, "~> 0.3"},
      {:nerves_network, "~> 0.3"},
      {:nerves_firmware_ssh, "~> 0.2"},
