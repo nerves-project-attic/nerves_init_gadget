6980a3887bb85c12e9f3ae2ee96a790c2a04d1d6
Greg Mefford <greg@gregmefford.com>
2018-08-05 14:10:45 -0400

    Use Elixir 1.5 module childspecs

--- lib/nerves_init_gadget/application.ex
+++ lib/nerves_init_gadget/application.ex
@@ -4,15 +4,12 @@ defmodule Nerves.InitGadget.Application do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
     config_opts = Map.new(Application.get_all_env(:nerves_init_gadget))
     merged_opts = Map.merge(%Nerves.InitGadget.Options{}, config_opts)
 
-    # Define workers and child supervisors to be supervised
     children = [
-      worker(Nerves.InitGadget.NetworkManager, [merged_opts]),
-      worker(Nerves.InitGadget.SSHConsole, [merged_opts])
+      {Nerves.InitGadget.NetworkManager, merged_opts},
+      {Nerves.InitGadget.SSHConsole, merged_opts}
     ]
 
     opts = [strategy: :one_for_one, name: Nerves.InitGadget.Supervisor]
