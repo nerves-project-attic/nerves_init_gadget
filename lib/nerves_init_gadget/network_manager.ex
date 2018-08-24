751929403930fe1b2d0a1b5b3a67766c28715c97
Greg Mefford <greg@gregmefford.com>
2018-08-24 11:47:45 -0400

    Remove duplicate ifname NetworkManager.State

--- lib/nerves_init_gadget/network_manager.ex
+++ lib/nerves_init_gadget/network_manager.ex
@@ -1,13 +1,12 @@
 defmodule Nerves.InitGadget.NetworkManager do
-  use GenServer
-
-  require Logger
-
   @moduledoc false
+
+  use GenServer
+  require Logger
 
   defmodule State do
     @moduledoc false
-    defstruct ifname: nil, ip: nil, opts: nil
+    defstruct ip: nil, opts: nil
   end
 
   def start_link(opts) do
@@ -23,24 +22,22 @@ defmodule Nerves.InitGadget.NetworkManager do
     init_mdns(opts.mdns_domain)
     init_net_kernel(opts)
 
-    {:ok, %State{ifname: opts.ifname, opts: opts}}
+    {:ok, %State{opts: opts}}
   end
 
   def handle_info({:system_registry, :global, registry}, state) do
-    new_ip = get_in(registry, [:state, :network_interface, state.ifname, :ipv4_address])
+    new_ip = get_in(registry, [:state, :network_interface, state.opts.ifname, :ipv4_address])
     handle_ip_update(state, new_ip)
   end
 
-  defp handle_ip_update(%{ip: old_ip} = state, new_ip) when old_ip == new_ip do
-    # No change
-    {:noreply, state}
-  end
+  # New IP address is same as current IP address
+  defp handle_ip_update(%{ip: ip} = state, ip), do: {:noreply, state}
 
   defp handle_ip_update(state, new_ip) do
-    Logger.debug("IP address for #{state.ifname} changed to #{new_ip}")
+    Logger.debug("IP address for #{state.opts.ifname} changed to #{new_ip}")
     update_mdns(new_ip, state.opts.mdns_domain)
     update_net_kernel(new_ip, state.opts)
-    {:noreply, %{state | ip: new_ip}}
+    {:noreply, %State{state | ip: new_ip}}
   end
 
   defp init_mdns(nil), do: :ok
