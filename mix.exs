983393b39a0c4a70f7d0df88211123ec5c2a31f1
Frank Hunleth <fhunleth@troodon-software.com>
2018-08-24 21:46:32 -0400

    Bump ex_docs and make runtime: false

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Nerves.InitGadget.MixProject do
       {:nerves_firmware_ssh, "~> 0.2"},
       {:mdns, "~> 1.0"},
       {:ring_logger, "~> 0.4"},
-      {:ex_doc, "~> 0.11", only: :dev}
+      {:ex_doc, "~> 0.19", only: [:dev, :test], runtime: false}
     ]
   end
 end
